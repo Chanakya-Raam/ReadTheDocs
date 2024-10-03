e8963d608166c043f45f68294f048c23b14b61f6
Chanakya-Raam <chanakya.terala20@st.niituniversity.in>
2024-10-03 15:10:42 +0530

    conf.py change

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,21 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'FastApi Documentation'
 copyright = '2024, Chanakya Terala'
 author = 'Chanakya Terala'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',       # Automatically document your code
+    'sphinx.ext.napoleon',      # For Google and NumPy style docstrings
+    'sphinx.ext.viewcode',      # Add links to highlighted source code
+    'sphinx_rtd_theme',         # Read the Docs theme
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'  # Changing the theme to Read the Docs
 html_static_path = ['_static']
